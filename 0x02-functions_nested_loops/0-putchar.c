d7c9d7e05ae9f0d53d67ce1195902fdf087e2f11
Enzeyi <emmanuelsiemaenzeyi@gmail.com>
2022-12-14 17:04:54 +0300

    Changing the commment styles, taking /n out of the parantheses

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-//0-putchar.c prints the word _putchar on the stdout
-//_putchar: The word to print
-//Return: 0
-//Function name: print_putchar
-//Return-type: int
-//parameter: char
+/*0-putchar.c prints the word _putchar on the stdout
+ *_putchar: The word to print
+ *Return: 0
+ *Function name: print_putchar
+ *Return-type: int
+ *parameter: char
+*/
 
 int print_putchar(char) {
-  //print function to write _putchar
-  printf('_putchar'\n)
-  //reurn=0
+  /*print function to write _putchar*/
+  printf('_putchar')\n
+  /*return=0*/
 
   return 0;
 }
