5bb5fb387523fadbe35ca99f53f4809e4becc43a
Enzeyi <emmanuelsiemaenzeyi@gmail.com>
2022-12-14 16:52:34 +0300

    Trial 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//0-putchar.c prints the word _putchar on the stdout
+//_putchar: The word to print
+//Return: 0
+//Function name: print_putchar
+//Return-type: int
+//parameter: char
+
+int print_putchar(char) {
+  //print function to write _putchar
+  printf('_putchar'\n)
+  //reurn=0
+
+  return 0;
+}
