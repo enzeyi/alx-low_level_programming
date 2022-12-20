2d90f7da4e4acebb8421a817d117864e16d979d3
Enzeyi <emmanuelsiemaenzeyi@gmail.com>
2022-12-20 16:02:48 +0300

    run

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,18 @@
+/*
+ * File: 3-puts.c
+ * Auth: Enzeyi
+ */
+
 #include "main.h"
 
 /**
- * print_rev - prints a string, in reverse, followed by a new line.
- * @s: input string.
- * Return: no return.
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
  */
-void print_rev(char *s)
+void print_rev(char *s);
 {
-	int count = 0;
+	while (*str)
+		_putchar(*str++);
 
-	while (count >= 0)
-	{
-		if (s[count] == '\0')
-			break;
-		count++;
-	}
-
-	for (count--; count >= 0; count--)
-		_putchar(s[count]);
 	_putchar('\n');
 }
