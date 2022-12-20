95344e8ebc1e5f1d280572f3450726bb5858f961
Enzeyi <emmanuelsiemaenzeyi@gmail.com>
2022-12-20 19:23:20 +0300

    hghg

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -19,7 +19,7 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-_putchar(char c).c
+int _putchar(char c);
 char *_strcpy(char *dest, char *src);
 
 #endif
