cbb347e26d8386960a419e0c79ac747723fb9761
Enzeyi <emmanuelsiemaenzeyi@gmail.com>
2022-12-21 16:52:38 +0300

    0x06. C - More pointers, arrays and strings Commit 1

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: enzeyi
+ * Desc: Contains all the prototypes in the directory: 0x06. C - More pointers, arrays and strings
+ */
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+int _putchar(char c);
+
+#endif
