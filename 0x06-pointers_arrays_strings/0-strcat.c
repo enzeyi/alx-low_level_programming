a41d63858a7353cb09ce01e741d13902b5030855
Enzeyi <emmanuelsiemaenzeyi@gmail.com>
2022-12-22 13:41:01 +0300

    hello

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -21,13 +21,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-    int index = 0, dest_len = 0;
+    size_t dest_len = strlen(dest);
+    size_t i;
 
-    while (dest[index++])
-        dest_len++;
+    for (i = 0; src[i] != '\0'; i++)
+        dest[dest_len + i] = src[i];
+    dest[dest_len + i] = '\0';
 
-    for (index = 0; src[index]; index++)
-        dest[dest_len++] = src[index];
-
-    return (dest);
+    return dest;
 }
