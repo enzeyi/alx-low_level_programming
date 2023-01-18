04706b2af1f1784c99bf8de2bfcf4271fd59ffec
enzeyi <emmanuelsiemaenzeyi@gmail.com>
2023-01-18 16:18:31 +0300

    updating

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,9 +1,13 @@
-/**FILE: 0-object_like_macro.h 
- * AUTH: Enzeyi
- * DESC: a header file that defines a macro named SIZE as an abbreviation for the token 1024
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Auth: enzeyi
+ * Desc: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
  */
 
 #define SIZE 1024
 
 #endif
-
